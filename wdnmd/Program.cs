0be6646f7f31523ba6c5422c6f41251be7c73912
username <1151555861@qq.com>
2021-06-18 20:00:41 +0800

    first commit

---
+++ wdnmd/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace wdnmd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           PrintNames(GenerateNames());
+           Console.ReadKey();
+        }
+         static string[] GenerateNames()
+       {
+           string[] names = new string[4];
+           names[0] = "Gamma";
+           names[1] = "Vlissides";
+           names[2] = "Johnson";
+           names[3] = "Helm";
+           return names;
+       }
+       static void PrintNames(String[] names)
+        {
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
